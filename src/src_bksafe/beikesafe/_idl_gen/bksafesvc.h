b8d2304d5b6d3683884fd93f795142bb34cb10a3
5455945 <5455945@gmail.com>
2018-05-26 20:24:29 +0800

    #0001 545 1 添加http://code.ijinshan.com/download/LIB4pcmanager.tbz压缩包，2 修改了部分工程文件，现在大部分工程可以编译通过，包括ksafe.sln的release版本，运行还有问题

--- src/src_bksafe/beikesafe/_idl_gen/bksafesvc.h
+++ src/src_bksafe/beikesafe/_idl_gen/bksafesvc.h
@@ -3,18 +3,18 @@
 /* this ALWAYS GENERATED file contains the definitions for the interfaces */
 
 
- /* File created by MIDL compiler version 7.00.0500 */
-/* at Wed Mar 16 17:07:26 2011
+ /* File created by MIDL compiler version 7.00.0555 */
+/* at Sat May 26 18:41:15 2018
  */
 /* Compiler settings for ..\..\publish\idl\bksafesvc.idl:
-    Oicf, W1, Zp8, env=Win32 (32b run)
+    Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
     protocol : dce , ms_ext, c_ext, robust
     error checks: allocation ref bounds_check enum stub_data 
     VC __declspec() decoration level: 
          __declspec(uuid()), __declspec(selectany), __declspec(novtable)
          DECLSPEC_UUID(), MIDL_INTERFACE()
 */
-//@@MIDL_FILE_HEADING(  )
+/* @@MIDL_FILE_HEADING(  ) */
 
 #pragma warning( disable: 4049 )  /* more than 64k source lines */
 
@@ -110,7 +110,7 @@ EXTERN_C const IID IID_Ibkcomm;
         HRESULT ( STDMETHODCALLTYPE *QueryInterface )( 
             Ibkcomm * This,
             /* [in] */ REFIID riid,
-            /* [iid_is][out] */ 
+            /* [annotation][iid_is][out] */ 
             __RPC__deref_out  void **ppvObject);
         
         ULONG ( STDMETHODCALLTYPE *AddRef )( 
--- src/src_bksafe/beikesafe/_idl_gen/bksafesvc_p.c
+++ src/src_bksafe/beikesafe/_idl_gen/bksafesvc_p.c
@@ -3,18 +3,18 @@
 /* this ALWAYS GENERATED file contains the proxy stub code */
 
 
- /* File created by MIDL compiler version 7.00.0500 */
-/* at Wed Mar 16 17:07:26 2011
+ /* File created by MIDL compiler version 7.00.0555 */
+/* at Sat May 26 18:41:15 2018
  */
 /* Compiler settings for ..\..\publish\idl\bksafesvc.idl:
-    Oicf, W1, Zp8, env=Win32 (32b run)
+    Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
     protocol : dce , ms_ext, c_ext, robust
     error checks: allocation ref bounds_check enum stub_data 
     VC __declspec() decoration level: 
          __declspec(uuid()), __declspec(selectany), __declspec(novtable)
          DECLSPEC_UUID(), MIDL_INTERFACE()
 */
-//@@MIDL_FILE_HEADING(  )
+/* @@MIDL_FILE_HEADING(  ) */
 
 #if !defined(_M_IA64) && !defined(_M_AMD64)
 
@@ -44,7 +44,7 @@
 #include "rpcproxy.h"
 #ifndef __RPCPROXY_H_VERSION__
 #error this stub requires an updated version of <rpcproxy.h>
-#endif // __RPCPROXY_H_VERSION__
+#endif /* __RPCPROXY_H_VERSION__ */
 
 
 #include "bksafesvc.h"
@@ -74,7 +74,7 @@ typedef struct _bksafesvc_MIDL_EXPR_FORMAT_STRING
     } bksafesvc_MIDL_EXPR_FORMAT_STRING;
 
 
-static RPC_SYNTAX_IDENTIFIER  _RpcTransferSyntax = 
+static const RPC_SYNTAX_IDENTIFIER  _RpcTransferSyntax = 
 {{0x8A885D04,0x1CEB,0x11C9,{0x9F,0xE8,0x08,0x00,0x2B,0x10,0x48,0x60}},{2,0}};
 
 
@@ -97,7 +97,7 @@ extern const USER_MARSHAL_ROUTINE_QUADRUPLE UserMarshalRoutines[ WIRE_MARSHAL_TA
 #endif
 
 #if !(TARGET_IS_NT50_OR_LATER)
-#error You need a Windows 2000 or later to run this stub because it uses these features:
+#error You need Windows 2000 or later to run this stub because it uses these features:
 #error   /robust command line switch.
 #error However, your C/C++ compilation flags indicate you intend to run this app on earlier systems.
 #error This app will fail with the RPC_X_WRONG_STUB_VERSION error.
@@ -340,9 +340,9 @@ CINTERFACE_PROXY_VTABLE(10) _IbkcommProxyVtbl =
     IUnknown_QueryInterface_Proxy,
     IUnknown_AddRef_Proxy,
     IUnknown_Release_Proxy ,
-    0 /* (void *) (INT_PTR) -1 /* IDispatch::GetTypeInfoCount */ ,
-    0 /* (void *) (INT_PTR) -1 /* IDispatch::GetTypeInfo */ ,
-    0 /* (void *) (INT_PTR) -1 /* IDispatch::GetIDsOfNames */ ,
+    0 /* IDispatch::GetTypeInfoCount */ ,
+    0 /* IDispatch::GetTypeInfo */ ,
+    0 /* IDispatch::GetIDsOfNames */ ,
     0 /* IDispatch_Invoke_Proxy */ ,
     (void *) (INT_PTR) -1 /* Ibkcomm::Initialize */ ,
     (void *) (INT_PTR) -1 /* Ibkcomm::Call */ ,
@@ -384,7 +384,7 @@ static const MIDL_STUB_DESC Object_StubDesc =
     1, /* -error bounds_check flag */
     0x50002, /* Ndr library version */
     0,
-    0x70001f4, /* MIDL Version 7.0.500 */
+    0x700022b, /* MIDL Version 7.0.555 */
     0,
     UserMarshalRoutines,
     0,  /* notify & notify_flag routine table */
@@ -394,13 +394,13 @@ static const MIDL_STUB_DESC Object_StubDesc =
     0
     };
 
-const CInterfaceProxyVtbl * _bksafesvc_ProxyVtblList[] = 
+const CInterfaceProxyVtbl * const _bksafesvc_ProxyVtblList[] = 
 {
     ( CInterfaceProxyVtbl *) &_IbkcommProxyVtbl,
     0
 };
 
-const CInterfaceStubVtbl * _bksafesvc_StubVtblList[] = 
+const CInterfaceStubVtbl * const _bksafesvc_StubVtblList[] = 
 {
     ( CInterfaceStubVtbl *) &_IbkcommStubVtbl,
     0
@@ -412,7 +412,7 @@ PCInterfaceName const _bksafesvc_InterfaceNamesList[] =
     0
 };
 
-const IID *  _bksafesvc_BaseIIDList[] = 
+const IID *  const _bksafesvc_BaseIIDList[] = 
 {
     &IID_IDispatch,
     0
